7de2ca46237852619b82f830689bd2625edaac36
ns loni <nsloni@gmail.com>
2025-05-14 11:50:13 +0530

    1-set-up the project

---
+++ ai-interview-resume/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+        <div>
+          <h1>NS LONI</h1>
+          <Button>Click On It</Button>
+        </div>
+  );
+}
